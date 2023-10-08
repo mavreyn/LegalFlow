a38d45abb605f5d4df2461a33a360b8454fa932a
Maverick Reynolds <mcreynolds02@gmail.com>
2023-10-07 20:37:23 -0400

    new

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ TODO
 - Edit Theme for Morgan Colors
 - Py thing to have langchain repeat until correct format
 
+- Questions necessary for the ChatBot
+
 - Chat with our virtual assistant, upload a document for more context
 
 https://www.youtube.com/watch?v=6fs80o7Xm4I&ab_channel=FaniloAndrianasolo
@@ -31,7 +33,9 @@ import streamlit as st
 from langchain.prompts import *
 from langchain import LLMChain
 import configparser
+
 import openai
+import re
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 
@@ -41,6 +45,7 @@ from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import PyPDFLoader
 from langchain.memory import ConversationSummaryMemory
+from langchain.chains.conversation.memory import ConversationSummaryMemory
 from langchain.chains import ConversationChain
 
 from dataclasses import dataclass
@@ -48,8 +53,6 @@ from typing import Literal
 
 from langchain import OpenAI
 from langchain.callbacks import get_openai_callback
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationSummaryMemory
 
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
@@ -83,6 +86,7 @@ class Message:
     origin: Literal["human", "llm"]
     message: str
 
+
 def initialize_session_state():
     if "history" not in st.session_state:
         st.session_state["history"] = []
@@ -105,40 +109,44 @@ def on_click_callback():
 
 def main():
     initialize_session_state()
-    load_css()
     
     # Begin the Streamlit App Here
-    st.title('MorganAI')
-    st.write('lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.')
-
+    st.title('LegalFlow')
+    st.subheader('Your assistant for document analysis and legal advice.')
+    st.write('LegalFlow was created by students participating in the UCF Knight Hacks 2023 Hackathon to help with document classification and extraction of information.')
 
     # Do the sidebar here
-    st.sidebar.title('Enter Document URL')
-    file = st.sidebar.file_uploader("Upload a file", type=["pdf", "png", "jpg", "jpeg"])
+    st.sidebar.title('Upload a Legal Document')
+    file = st.sidebar.file_uploader("Upload a file here", type=["pdf", "png", "jpg", "jpeg"])
+
+    # Add information for the file
     if file:
-        document_analysis_client = DocumentAnalysisClient(
-            endpoint=azure_endpoint,
-            credential=AzureKeyCredential(azure_key)
-        )
-
-        poller = document_analysis_client.begin_analyze_document(
-            "prebuilt-receipt", file
-        )
-
+        with st.spinner('Analyzing your document...'):
+            document_analysis_client = DocumentAnalysisClient(
+                endpoint=azure_endpoint,
+                credential=AzureKeyCredential(azure_key)
+            )
+            poller = document_analysis_client.begin_analyze_document(
+                "prebuilt-receipt", file
+            )
+        
         result = poller.result()
-        st.sidebar.write(result.content)
+        st.sidebar.success('Document uploaded successfully')
     else:
-        st.warning('Please upload a file for further analysis')
+        st.warning('Please use the sidebar to upload a document for further environment, or chat with the LegalFlow assistant for general questions')
 
               
     st.markdown('---')
 
     if file:
-        st.success(f'Document type: {DOCUMENT_TYPES[int(get_type_of_document(result.content)) - 1]}')
+        st.subheader('Document Analysis')
+        st.write('We have extracted some information from your document: ')
+
+        st.write(get_type_of_document(result.content))
 
     st.markdown('---')
 
-    st.write('Hi, I am MorganAI. I am here to help you with your legal needs.')
+    st.write('Hello, I am LegalFlowChat and I am here to help you with any questions you may have about your legal documents. Please use the chatbox below to ask any questions you may have.')
     chat_palceholder = st.container()
     prompt_placeholder = st.form("Chat-form")
 
@@ -173,11 +181,21 @@ def get_vectorstore(source_doc):
 def get_type_of_document(document_text: str) -> str:
     template = """You are an AI legal agent that is working at an injury law firm to classify certain documents. You are given a document and you need to classify it into one of the following categories:\n
 
-    """ + "".join([f"{i+1}. {doc_type}\n" for i, doc_type in enumerate(DOCUMENT_TYPES)]) + """\nThe document you are given (by the user) has been put through an OCR system to convert it from an image to text. The OCR system is not perfect and there may be some errors in the text. Only use the information from the document, and limit your answer to only a single number (e.g. "1"..."7" without the quotes). The user will provide all information from the document between triple backticks in their prompt."""
+    """ + "".join([f"{i+1}. {doc_type}\n" for i, doc_type in enumerate(DOCUMENT_TYPES)]) + """\nThe document you are given (by the user) has been put through an OCR system to convert it from an image to text. The OCR system is not perfect and there may be some errors in the text. The user will provide all information from the document between triple backticks in their prompt.
+    
+    Your answer should be just a single integer response. Here is an example:
+    
+    User: ```<<<DOCUMENT INFORMATION>>>```
+    AI Agent: 4
+    
+    User: ```<<<DOCUMENT2 INFORMATION>>>```
+    AI Agent: 2
+    
+    Ensure you follow proper formatting as you use your best judgement to classify the document."""
 
     prompt = ChatPromptTemplate.from_messages([
         SystemMessagePromptTemplate.from_template(template),
-        HumanMessagePromptTemplate.from_template("Here is the documentation: ```{document_text}```"),
+        HumanMessagePromptTemplate.from_template("```{document_text}```"),
     ])
 
     chain = LLMChain(
@@ -187,6 +205,12 @@ def get_type_of_document(document_text: str) -> str:
     )
 
     response = chain.run(document_text=document_text)
+    try:
+        st.write(response)
+        response = int(re.search(r'\d', response).group(0)) # Convert to int
+        response = DOCUMENT_TYPES[response - 1]             # Convert to document type
+    except:
+        pass
     return response
 
 
